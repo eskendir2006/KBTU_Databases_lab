7061f90d5f3cb7a0fd3708cdfb897aaf17d8c1b5
Yeskendir Ussibaliyev <59607469+eskendir2006@users.noreply.github.com>
2024-09-25 00:36:40 +0500

    Create lab1.sql

---
+++ lab1.sql
@@ -0,0 +1,101 @@
+CREATE database lab1;
+CREATE TABLE airline (
+    airline_id INT PRIMARY KEY NOT NULL,
+    airline_code VARCHAR(30) NOT NULL,
+    airline_name VARCHAR(50) NOT NULL,
+    airline_country VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE airport (
+    airport_id INT PRIMARY KEY NOT NULL,
+    airport_name VARCHAR(50) NOT NULL,
+    country VARCHAR(50) NOT NULL,
+    state VARCHAR(50) NOT NULL,
+    city VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE baggage_check (
+    baggage_check_id INT PRIMARY KEY NOT NULL,
+    check_result VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    booking_id INT NOT NULL,
+    passenger_id INT NOT NULL
+);
+
+CREATE TABLE baggage (
+    baggage_id INT PRIMARY KEY NOT NULL,
+    weight_in_kg DECIMAL(4,2) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    booking_id INT NOT NULL
+);
+
+CREATE TABLE boarding_pass (
+    boarding_pass_id INT PRIMARY KEY NOT NULL,
+    booking_id INT NOT NULL,
+    seat VARCHAR(50) NOT NULL,
+    boarding_time TIMESTAMP NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE booking_flight (
+    booking_flight_id INT PRIMARY KEY NOT NULL,
+    booking_id INT NOT NULL,
+    flight_id INT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE booking (
+    booking_id INT PRIMARY KEY NOT NULL,
+    flight_id INT NOT NULL,
+    passenger_id INT NOT NULL,
+    booking_platform VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    status VARCHAR(50) NOT NULL,
+    ticket_price DECIMAL(7,2) NOT NULL
+);
+
+CREATE TABLE flights (
+    flight_id INT PRIMARY KEY NOT NULL,
+    sch_departure_time TIMESTAMP NOT NULL,
+    sch_arrival_time TIMESTAMP NOT NULL,
+    departing_airport_id INT NOT NULL,
+    arriving_airport_id INT NOT NULL,
+    departing_gate TEXT NOT NULL, 
+    arriving_gate VARCHAR(50) NOT NULL,
+    airline_id INT NOT NULL,
+    act_departure_time TIMESTAMP,
+    act_arrival_time TIMESTAMP,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE passengers (
+    passenger_id INT PRIMARY KEY NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    gender VARCHAR(50) NOT NULL,
+    country_of_citizenship VARCHAR(50) NOT NULL,
+    country_of_residence VARCHAR(50) NOT NULL,
+    passport_number VARCHAR(20) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE security_check (
+    security_check_id INT PRIMARY KEY NOT NULL,
+    check_result VARCHAR(20) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    passenger_id INT NOT NULL
+);
+DROP database lab1;
